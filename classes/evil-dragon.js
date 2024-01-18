39adfc087628fd0810bdc59cd90cbf0fa2faef97
annon-prog <fidelisdela.wambui@gmail.com>
2024-01-18 14:40:28 +0300

    Task: Final submission

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,27 @@
-// Your code here
+//Add an instance burnsNemesis()
+//return ${name} destroys ${nemesis} with fire! WHOOSH!!!
+//Export the class.
+
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon{
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        let evilDoingsArr = this.evilDoings;
+
+        evilDoingsArr.forEach(evilDoing => {
+            console.log(`${this.name} will ${evilDoing}`);
+        })
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+}
+
+module.exports = EvilDragon;
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,27 @@
-// Your code here
+
+//We should export the FriendlyDragon class
+
+const Dragon = require('./dragon');
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        let lifeGoalsArr = this.lifeGoals;
+
+        lifeGoalsArr.forEach(lifeGoal => {
+            console.log(`${this.name} likes to ${lifeGoal}`)
+        });
+    }
+    
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`;
+    }
+
+}
+
+module.exports = FriendlyDragon;
